1db851cca393f6253a348fc44d5741d6b6a3c1fb
RubenCali <ruben.cali.2003@gmail.com>
2021-01-08 11:57:34 +0100

    af

---
+++ index.js
@@ -0,0 +1,46 @@
+let alleLinks = document.querySelectorAll("nav a");
+
+let deSecties = document.querySelectorAll('section');
+const opties = {
+    // rootMargin: '-150px',
+    treshold: 1.0
+};
+const verwerkDoorsnijding = (entries, observer) => {
+    entries.forEach(entry => {
+        console.log(entry.target.parentNode.id + "doorsnijdt" + entry.isIntersecting);
+        if(entry.isIntersecting){
+            let link = zoekBijpassendeLink('#' + entry.target.parentNode.id);
+            maakActief(link);
+        }
+    })
+    console.log(entry)
+}
+let observer = new IntersectionObserver(verwerkDoorsnijding, opties);
+
+// observer.observe(deSecties[1]);
+deSecties.forEach ( sectie => {
+    observer.observe(sectie.getElementsByTagName('p')[0]);
+})
+
+const verwijderActief = () => {
+  alleLinks.forEach((link) => {
+    if ((link.classList = "actief")) {
+        link.classList.remove('actief');
+    }
+  });
+}
+const maakActief =  (elem) => {
+    verwijderActief();
+    elem.classList.add('actief');
+}
+alleLinks.forEach( (link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        maakActief(e.target);
+        window.location = e.target.href;
+    })
+})
+const zoekBijpassendeLink = (id) => {
+    let link = document.querySelector('nav a[href="'+ id +'"]');
+    return link;
+}
